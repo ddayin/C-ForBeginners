c39b8be4238cda9d0861a35c6b03901330a09bce
ddayin <ddayin@hotmail.com>
2025-01-04 09:37:39 +0900

    test

---
+++ main_.c
@@ -0,0 +1,16 @@
+﻿#include <stdio.h>
+
+int main()
+{
+    int out = Process(1);
+    //printf(out);
+    return out;
+}
+
+int Process(int input)
+{
+    int output = input + 1;
+    printf("Hello Geek! %d", output);
+
+    return output;
+}
